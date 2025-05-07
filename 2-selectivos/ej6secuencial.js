63b0fa8c295d2bf37b3ce285110beab98e007143
Erick <erick.nahuelpaiva.001@gmail.com>
2025-05-07 19:25:40 -0300

    ej 6 agregado

--- 2-selectivos/ej6secuencial.js
+++ 2-selectivos/ej6secuencial.js
@@ -1,7 +1,28 @@
 const leer = require("prompt-sync")();
 
+const RANGO_MAX = 10;
+
+
 function main() {
-     
+    let numeroAleatorio = -1;
+    let numeroIngresado = -1;
+
+    numeroAleatorio = azar(RANGO_MAX);
+
+    console.log("Ingresa un numero entre 0 y " + RANGO_MAX);
+    numeroIngresado = Number(leer());
+
+    if (numeroIngresado < numeroAleatorio) {
+        console.log("CARA");
+    } else if (numeroIngresado >= numeroAleatorio) {
+        console.log("CRUZ");
+    }
 }
 
+
+
 main();
+
+function azar(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
